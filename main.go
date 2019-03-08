e108ef727b85a36f09b43d720bacecf0c7cb1a34
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-03-08 17:00:51 -0500

    parsing cli flags

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	flag "github.com/ogier/pflag"
+)
+
+// flags
+var (
+	force bool
+)
+
 func main() {
-	fmt.Println("hello world")
+	flag.Parse()
+	if force {
+		fmt.Println("you forced it")
+	} else {
+		fmt.Println("not forced")
+	}
+}
+
+func init() {
+	flag.BoolVarP(&force, "force", "f", false, "Force delete")
 }
