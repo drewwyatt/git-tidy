9be0a40ed26248625326b834a387a367d32ef2cc
Drew Wyatt <drew.wyatt@onepeloton.com>
2019-03-08 22:59:52 -0500

    can execute commands

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	flag "github.com/ogier/pflag"
 )
@@ -11,13 +13,27 @@ var (
 	force bool
 )
 
+func checkForError(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	flag.Parse()
-	if force {
-		fmt.Println("you forced it")
-	} else {
-		fmt.Println("not forced")
-	}
+
+	cmd := "git"
+	args := []string{"status"}
+
+	out, err := exec.Command(cmd, args...).Output()
+	checkForError(err)
+
+	// Can I combine these?
+	formattedOutput := fmt.Sprintf("%s", out)
+	fmt.Println(formattedOutput)
+
+	fmt.Println("Done.")
 }
 
 func init() {
