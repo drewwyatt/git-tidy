27cdfaaece608cc4a894d9d8c2ba45a8f879d00c
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-04-30 19:18:10 -0400

    added test for ListRemoteBranches

--- git/git_test.go
+++ git/git_test.go
@@ -2,8 +2,9 @@ package git_test
 
 import (
 	"bytes"
-	g "github.com/drewwyatt/gitclean/git"
 	"testing"
+
+	g "github.com/drewwyatt/gitclean/git"
 )
 
 func TestFetch(t *testing.T) {
@@ -22,9 +23,38 @@ func TestFetch(t *testing.T) {
 	git.Fetch()
 	callsToCommand := len(mockedExecutor.CommandCalls())
 	if callsToCommand != 1 {
-		t.Errorf("Send was called %d times", callsToCommand)
+		t.Errorf("cmd was called %d times", callsToCommand)
 	}
 	if cmdName != "fetch" {
 		t.Errorf("unexpected command name: %s", cmdName)
+	}
+}
+
+func TestListRemoteBranches(t *testing.T) {
+	var cmdName string
+	var arg string
+	mockedExecutor := &g.ExecutorMock{
+		CommandFunc: func(name string, otherArgs ...string) g.CmdRunner {
+			cmdName = otherArgs[2]
+			arg = otherArgs[3]
+			return &g.CmdRunnerMock{
+				RunFunc: func(o *bytes.Buffer, e *bytes.Buffer) error {
+					return nil
+				},
+			}
+		},
+	}
+	git := g.NewGit(".", mockedExecutor)
+	git.ListRemoteBranches()
+	callsToCommand := len(mockedExecutor.CommandCalls())
+	if callsToCommand != 1 {
+		t.Errorf("cmd was called %d times", callsToCommand)
+	}
+	if cmdName != "branch" {
+		t.Errorf("unexpected command name: %s", cmdName)
+	}
+
+	if arg != "-vv" {
+		t.Errorf("unexpected arg: %s", arg)
 	}
 }
