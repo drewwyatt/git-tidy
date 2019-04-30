86acf602581dda9b96ce7bcb4972af17b1df244c
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-04-30 19:30:49 -0400

    added Delete test

--- git/git_test.go
+++ git/git_test.go
@@ -7,6 +7,45 @@ import (
 	g "github.com/drewwyatt/gitclean/git"
 )
 
+func TestDelete(t *testing.T) {
+	var cmdName string
+	var deletedBranch string
+	var deletionArg string
+	branchName := "fix-the-bug"
+	mockedExecutor := &g.ExecutorMock{
+		CommandFunc: func(name string, otherArgs ...string) g.CmdRunner {
+			cmdName = otherArgs[2]
+			deletionArg = otherArgs[3]
+			deletedBranch = otherArgs[4]
+			return &g.CmdRunnerMock{
+				RunFunc: func(o *bytes.Buffer, e *bytes.Buffer) error {
+					return nil
+				},
+			}
+		},
+	}
+	git := g.NewGit(".", mockedExecutor)
+	git.Delete(branchName, false)
+	callsToCommand := len(mockedExecutor.CommandCalls())
+
+	if callsToCommand != 1 {
+		t.Errorf("cmd was called %d times", callsToCommand)
+	}
+	if cmdName != "branch" {
+		t.Errorf("unexpected command name: %s (expected 'branch')", cmdName)
+	}
+	if deletedBranch != branchName {
+		t.Errorf("unexpected branch name: %s (expected %s)", deletedBranch, branchName)
+	}
+	if deletionArg != "-d" {
+		t.Errorf("unexpected deletion arg: %s (expected '-d')", deletionArg)
+	}
+
+	git.Delete(branchName, true)
+	if deletionArg != "-D" {
+		t.Errorf("unexpected deletion arg: %s (expected '-D)", deletionArg)
+	}
+}
 func TestFetch(t *testing.T) {
 	var cmdName string
 	mockedExecutor := &g.ExecutorMock{
