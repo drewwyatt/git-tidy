67d4da0d5ff080e823066e059050dc94c9a0ca72
Drew Wyatt <drew.j.wyatt@gmail.com>
2019-05-13 23:04:13 -0400

    Using BranchManager in default.go

--- commands/default.go
+++ commands/default.go
@@ -2,35 +2,26 @@ package commands
 
 import (
 	"fmt"
-	"regexp"
 
+	"github.com/drewwyatt/git-tidy/git"
 	"gopkg.in/AlecAivazis/survey.v1"
-
-	gUtils "github.com/drewwyatt/git-tidy/git"
 )
-
-var goneBranch = regexp.MustCompile(`(?m)^(?:\*| ) ([^\s]+)\s+[a-z0-9]+ \[[^:\n]+: gone\].*$`)
 
 func Default(directory string, interactive bool, force bool) {
 	if directory == "" {
 		directory = "."
 	}
 
-	git := gUtils.NewGit(directory, gUtils.NewExecutorWithExec())
+	branchManager := git.NewBranchManager(directory, git.NewExecutorWithExec())
+	branchManager.Init()
 
 	goneBranches := []string{}
 	branchesToDelete := []string{}
 
-	git.Fetch().Prune().ListRemoteBranches()
-	submatches := goneBranch.FindAllStringSubmatch(git.Output, -1)
-	for _, matches := range submatches {
-		if len(matches) == 2 && matches[1] != "" {
-			if interactive {
-				goneBranches = append(goneBranches, matches[1])
-			} else {
-				branchesToDelete = append(branchesToDelete, matches[1])
-			}
-		}
+	if interactive {
+		goneBranches = branchManager.GoneBranches
+	} else {
+		branchesToDelete = branchManager.GoneBranches
 	}
 
 	prompt := &survey.MultiSelect{
@@ -40,19 +31,19 @@ func Default(directory string, interactive bool, force bool) {
 	survey.AskOne(prompt, &branchesToDelete, nil)
 
 	for _, branch := range branchesToDelete {
-		git.Delete(branch, force)
+		branchManager.Delete(branch, force)
 	}
 
-	if len(git.DeletedBranches) > 0 {
+	if len(branchManager.DeletedBranches) > 0 {
 		fmt.Println("Deleted branches:")
-		for _, branch := range git.DeletedBranches {
+		for _, branch := range branchManager.DeletedBranches {
 			fmt.Println(branch)
 		}
 	}
 
-	if len(git.BranchDeletionErrors) > 0 {
+	if len(branchManager.BranchDeletionErrors) > 0 {
 		fmt.Println("Errors:")
-		for _, err := range git.BranchDeletionErrors {
+		for _, err := range branchManager.BranchDeletionErrors {
 			fmt.Printf("[%s]: %s", err.Branch, err.Msg)
 		}
 	}
