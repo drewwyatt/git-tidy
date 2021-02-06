4fb06afbe8caa999957031900a9d276fadf781ff
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-02-06 13:39:41 -0500

    Fix issue with Prompt implementation for branches

--- src/prompt.rs
+++ src/prompt.rs
@@ -4,15 +4,19 @@ pub struct Prompt {}
 
 impl Prompt {
     pub fn with(branches: Vec<String>) -> Vec<String> {
-        let selections = MultiSelect::with_theme(&ColorfulTheme::default())
+        let mut branches = branches;
+        let mut selections = MultiSelect::with_theme(&ColorfulTheme::default())
             .with_prompt("Stale branches")
             .items(&branches)
             .interact()
             .unwrap();
 
+        // Sort by descending order
+        selections.sort_by(|a, b| b.cmp(a));
         selections
             .into_iter()
-            .map(|index| branches[index])
+            .map(|idx| branches.swap_remove(idx))
+            .rev() // sort back to ascending order
             .collect()
     }
 }
