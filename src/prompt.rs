087a340fcff09467160505b97a581b1f7caefee6
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-06 22:48:48 -0500

    this does not need to be reversed

--- src/prompt.rs
+++ src/prompt.rs
@@ -5,17 +5,18 @@ pub struct Prompt {}
 impl Prompt {
     pub fn with(branches: Vec<String>) -> Vec<String> {
         let mut branches = branches;
-        let mut selections = MultiSelect::with_theme(&ColorfulTheme::default())
+        let selections = MultiSelect::with_theme(&ColorfulTheme::default())
             .with_prompt("Stale branches")
             .items(&branches)
             .interact()
             .unwrap();
 
-        // Sort by descending order
-        selections.sort_by(|a, b| b.cmp(a));
         selections
             .into_iter()
-            .map(|idx| branches.swap_remove(idx))
+            .map(|idx| {
+                println!("removing idx: {} from: {:?}", idx, branches);
+                branches.swap_remove(idx)
+            })
             .rev() // sort back to ascending order
             .collect()
     }
