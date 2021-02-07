6d20acfb60940ebb7c12131898620235e2907d1a
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-07 10:07:23 -0500

    WIP

--- src/prompt.rs
+++ src/prompt.rs
@@ -3,11 +3,11 @@ use dialoguer::{theme::ColorfulTheme, MultiSelect};
 pub struct Prompt {}
 
 impl Prompt {
-    pub fn with(branches: Vec<String>) -> Vec<String> {
+    pub fn with(branches: &Vec<String>) -> Vec<String> {
         let mut branches = branches;
         let selections = MultiSelect::with_theme(&ColorfulTheme::default())
             .with_prompt("Stale branches")
-            .items(&branches)
+            .items(branches)
             .interact()
             .unwrap();
 
