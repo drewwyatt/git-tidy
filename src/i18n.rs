b454f7ac022df41de57f3f87027f11d7cdedf82d
Dalton Claybrook <daltonclaybrook@gmail.com>
2021-02-07 13:31:27 -0500

    Dalton/i18n enum (#41)
    * whoops
    
    * Example of i18n enum
    
    * spacing
    
    * move more copy to i18n file
    
    Co-authored-by: Drew Wyatt <drew.j.wyatt@gmail.com>

---
+++ src/i18n.rs
@@ -0,0 +1,33 @@
+use std::string::ToString;
+
+pub enum Text<'a> {
+    BranchesDeleted,
+    BranchesToDelete,
+    DeletingBranch(&'a str),
+    DeletingBranches,
+    DryRunEnabled,
+    FinishedWithErrors,
+    NoBranchesDeleted,
+    NothingToDo,
+    StartupMessage,
+    UnknownErrorEncountered,
+}
+
+impl<'a> ToString for Text<'a> {
+    fn to_string(&self) -> String {
+        match self {
+            Text::BranchesDeleted => "Branches deleted:".into(),
+            Text::BranchesToDelete => "Branches to delete:".into(),
+            Text::DeletingBranch(name) => format!("Deleting ‘{}’...", name),
+            Text::DeletingBranches => "Deleting branches...".into(),
+            Text::DryRunEnabled => {
+                "📣 NOTE: --dry-run enabled, no branches will be deleted.".into()
+            }
+            Text::FinishedWithErrors => "Finished with errors:".into(),
+            Text::NoBranchesDeleted => "No branches were deleted.".into(),
+            Text::NothingToDo => "Nothing to do!".into(),
+            Text::StartupMessage => "Tidying up...".into(),
+            Text::UnknownErrorEncountered => "An unknown error was encountered".into(),
+        }
+    }
+}
